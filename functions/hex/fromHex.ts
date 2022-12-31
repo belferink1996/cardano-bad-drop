5a29c3e52f7914b8e94f35776bf48576c0c0aadf
Ben Elferink <ben.elferink@icloud.com>
2022-12-31 16:32:37 +0200

    fix bad hex function

--- functions/hex/fromHex.ts
+++ functions/hex/fromHex.ts
@@ -1,6 +1,10 @@
 const fromHex = (hex: string): string => {
-  // @ts-ignore
-  return decodeURIComponent('%' + hex.match(/.{1,2}/g).join('%'))
+  try {
+    // @ts-ignore
+    return decodeURIComponent('%' + hex.match(/.{1,2}/g).join('%'))
+  } catch (error) {
+    return hex
+  }
 }
 
 export default fromHex
